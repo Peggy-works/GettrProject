58ff6ca73ea5742e5284da38dbc785e2884f1978
kikijee <84474876+kikijee@users.noreply.github.com>
2023-04-23 17:07:04 -0700

    yes

--- gettr-backend/src/main/java/com/example/gettrproject/entity/Postings.java
+++ gettr-backend/src/main/java/com/example/gettrproject/entity/Postings.java
@@ -2,7 +2,10 @@ package com.example.gettrproject.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "postings")
@@ -12,4 +15,51 @@ public class Postings extends AbstractEntity{
     private String title;
     private String description;
     private int likes;
+    private LocalDate postTime;
+    @ManyToOne(optional = false) //cannot have a post without user
+    private User author;
+
+    // getters and setters
+
+
+    public LocalDate getPostTime() {
+        return postTime;
+    }
+
+    public void setPostTime(LocalDate postTime) {
+        this.postTime = postTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 }
