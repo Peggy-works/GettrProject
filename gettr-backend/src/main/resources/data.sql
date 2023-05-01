781a0cac6f1ff1e43a5db5c01ccbd461252f8037
Christian Manibusan <cam007mani@gmail.com>
2023-05-01 11:34:38 -0700

    updated data.sql file

--- gettr-backend/src/main/resources/data.sql
+++ gettr-backend/src/main/resources/data.sql
@@ -1,5 +1,6 @@
 delete from app_user
 delete from messages_map
+delete from postings
 insert into app_user (id, username,name,hashed_password,connected,role) values ('1','cmani','Christian','$2a$12$0SP0lTe0eT',false,"USER")
 insert into app_user (id, username,name,hashed_password,connected,role) values ('2','bruce001','Bruce','$2a$12$0SP0lTe0eT',false,"USER")
 insert into app_user (id, username,name,hashed_password,connected,role) values ('3','kikijee','Marytez','$2a$12$0SP0lTe0eT',false,"USER")
