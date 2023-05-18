fae2a4542bcaf34ecc0452dfb7467a41ead79408
ethan <eth4n1@gmail.com>
2023-05-17 21:51:02 -0700

    Updated style.css and sign up

--- Gettr/src/components/Signup.js
+++ Gettr/src/components/Signup.js
@@ -1,23 +1,20 @@
 import { useEffect, useState, useRef } from 'react';
-import { signupFields } from "../constants/formFields"
-import Input from "./Input";
 import * as React from 'react';
+import Popup from './Popup'
+import Avatar from '@mui/material/Avatar';
 import { register } from '../api/AuthApi.js'
 import PasswordMeter from "./PasswordMeter";
-import { useNavigate } from 'react-router-dom'; 
-
+import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import TungstenOutlinedIcon from '@mui/icons-material/TungstenOutlined';
 
 
 function Copyright(props) {
@@ -33,9 +30,10 @@ function Copyright(props) {
   );
 }
 
+//Regex to set valid names
 const emailReg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[Ee][Dd][Uu]$/;
 const passReg = /^(?=.*[a-z])(?=.*[A-Z)(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
-
+const userReg = /^[a-zA-Z][a-zA-Z0-9]{4,24}$/
 
 const theme = createTheme();
 
@@ -46,8 +44,11 @@ const navigate = useNavigate();
 const userRef = useRef();
 const errRef = useRef();
 
+//To set the username
 const [user, setUser] = useState('');
 const [userFocus, setUserFocus] = useState(false);
+//To set t
+const [validUser, setValidUser] = useState(false);
 
 const [fName, setFName] = useState('');
 const [lName, setLName] = useState('');
@@ -59,6 +60,11 @@ const [emailFocus, setEmailFocus]  = useState(false);
 const [email,setEmail] = useState("");
 const [validEmail, setValidEmail] = useState(false);
 const [message, setMessage] = useState("");
+
+//For the popup button
+const [isOpenTerms, setIsOpenTerms] = useState(false);
+
+const [isOpenPrivacy, setIsOpenPrivacy] = useState(false);
 
 const [matchPwd, setMatchPwd] = useState('');
 const [validMatch, setValidMatch] = useState(false);
@@ -76,6 +82,14 @@ useEffect(() => {
     userRef.current.focus();
 }, [])
 
+const togglePopup = () => {
+    setIsOpenTerms(!isOpenTerms);
+}
+
+const togglePopup2 = () => {
+    setIsOpenPrivacy(!isOpenPrivacy);
+}
+
 //Validate email to see if it ends in .edu
 useEffect(() => {
     const result = emailReg.test(email);
@@ -83,6 +97,15 @@ useEffect(() => {
     console.log(email);
     setValidEmail(result)
 }, [email])
+
+
+//Validate username to make sure it's in
+useEffect(() => {
+    const result = userReg.test(user);
+    console.log(result);
+    console.log(user)
+    setValidUser(result)
+}, [user])
 
 //Validate the password with requirements: Length 8, 1 lowercase, 1 lowercase, 1 symbol
  const onChange = (e) => {
@@ -142,10 +165,10 @@ useEffect(() => {
       firstName: data.get('firstName'),
       lastName: data.get('lastName'),
       username: data.get('username'),
-    }); 
+    });
     //API call to AuthApi.js
     register(data.get('username'), data.get('password'), data.get('firstName') + data.get('lastName'))
-         .then(response => {console.log(response.data.token); navigate("/");})
+         .then(response => {console.log(response.data.token); navigate("/#");})
          .catch(error => console.log(error));
 
 
@@ -164,8 +187,8 @@ useEffect(() => {
             alignItems: 'center',
           }}
         >
-          <TungstenOutlinedIcon sx={{ m: 1, size: "large" }}/>
-          
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          </Avatar>
           <Typography component="h1" variant="h4">
            Join the Community
           </Typography>
@@ -198,8 +221,8 @@ useEffect(() => {
 
            />
            </Grid>
-           </Grid>
-            <Grid container spacing={2}>
+
+
               <Grid item xs={12}>
                 <TextField
                   required
@@ -248,13 +271,137 @@ useEffect(() => {
               <PasswordMeter password={pwdInput.password} actions={initPwdInput} />
               <Grid item xs={11}>
               </Grid>
+
               <Typography component="caption">
-                 By clicking Sign Up, you are agreeing to the <Link href="/#" variant="body2"> Terms of Use </Link>and you are acknowledging the <Link href="/#" variant="body2"> Privacy Policy </Link>
+                 By clicking Sign Up, you are agreeing to the
+                 {" "}
+                  <input
+                    type = "button"
+                    value = "Terms and Conditions"
+                    onClick = {togglePopup}
+                  />
+                 {" "}
+                    and you are acknowledging the
+                 {" "}
+                 <input
+                    type = "button"
+                    value = "Privacy Policy"
+                    onClick = {togglePopup2}
+                 />
               </Typography>
+
+              {isOpenTerms &&  <Popup
+                content = {<>
+                          <h2 style={{textAlign: "center"}}>Terms and Conditions</h2>
+                           <p>
+                           Welcome to Gettr. Before using our platform, please read the following terms and conditions carefully. By using our website, you agree to these terms and conditions.
+                           </p>
+                           <p>
+                           <br/>
+                           1. User Conduct: You must use our forum in a responsible and respectful manner. Do not post any content that is defamatory, abusive, obscene, vulgar, or harmful. You must respect the privacy and personal information of others and comply with all applicable laws and regulations.
+                           </p>
+                           <p>
+                           <br/>
+                           2. Intellectual Property: You must respect the intellectual property rights of others. Do not post any content that violates the copyright, trademark, or other proprietary rights of any person or entity.
+                           </p>
+                           <p>
+                           <br/>
+                           3. User Content: By posting content on our forum, you grant us a non-exclusive, transferable, sub-licensable, royalty-free, worldwide license to use, modify, distribute, and display your content on our website.
+                           </p>
+                           <p>
+                           <br/>
+                           4. Moderation: We reserve the right to moderate the content posted on our forum. We may remove or edit any content that we deem inappropriate or in violation of these terms and conditions. We may also suspend or terminate your access to our forum at any time, for any reason, without prior notice.
+                           </p>
+                           <p>
+                           <br/>
+                           5. Disclaimer of Warranties: Our forum is provided "as is" and we make no warranties, express or implied, about the accuracy, reliability, completeness, or timeliness of any information or content posted on our website.
+                           </p>
+                           <p>
+                           <br/>
+                           6. Limitation of Liability: We shall not be liable for any damages, including but not limited to, direct, indirect, incidental, special, or consequential damages arising out of or in connection with your use of our forum.
+                           </p>
+                           <p>
+                           <br/>
+                           7. Indemnification: You agree to indemnify and hold us harmless from any claim or demand, including reasonable attorneys' fees, made by any third party due to or arising out of your use of our forum, your violation of these terms and conditions, or your violation of any rights of another person or entity.
+                           </p>
+                           <p>
+                           <br/>
+                           8. Governing Law: These terms and conditions shall be governed by and construed in accordance with the laws of the jurisdiction in which we are based. Any legal action or proceeding arising out of or in connection with these terms and conditions shall be brought in the courts of that jurisdiction.
+                           </p>
+                           <p>
+                           <br/>
+                           9. Changes to Terms and Conditions: We reserve the right to modify these terms and conditions at any time. Your continued use of our forum after any such changes shall constitute your acceptance of the modified terms and conditions.
+                           </p>
+                           <p>
+                           <br/>
+                           10. Entire Agreement: These terms and conditions constitute the entire agreement between you and us regarding your use of our forum, and supersede all prior agreements and understandings, whether written or oral.
+                           </p>
+                        </>}
+                handleClose = {togglePopup}
+              />}
+
+              {isOpenPrivacy && <Popup
+                content = {<>
+                        <h2 style={{textAlign: "center"}}>Privacy Policy</h2>
+                        <p>
+                            Gettr  is committed to protecting the privacy of our users, including students and faculty members. This privacy policy explains how we collect, use, and protect your personal information when you use our website.
+                        </p>
+                        <p>
+                        <br/>
+                        <b>   Information We Collect: </b>
+                        </p>
+                        <p>
+                           When you register for our forum posting website, we collect personal information such as your name, email address, and university affiliation. We also collect information about your posts, comments, and other contributions to the website.
+                        </p>
+                        <p>
+                           We may also collect information about your device and internet connection, including your IP address, browser type, and operating system.
+                        </p>
+                        <p>
+                        <br/>
+                        <b>   How We Use Your Information: </b>
+                        </p>
+                        <p>
+                           We use your personal information to identify you as a user of our website, to provide you with access to our forums, and to communicate with you about your account and our services.
+
+
+                           We may also use your information to analyze user behavior and improve our website and services. We will not share your personal information with third parties for marketing purposes.
+                        </p>
+                        <p>
+                        <br/>
+                          <b> Data Security: </b>
+                        </p>
+                           We take appropriate measures to protect your personal information from unauthorized access, disclosure, or alteration. We use industry-standard security technologies and procedures to safeguard your information.
+
+                           However, no data transmission over the internet can be guaranteed to be 100% secure. Therefore, while we strive to protect your personal information, we cannot guarantee the security of any information you transmit to us.
+                        <p>
+                        <br/>
+                           <b>Cookies:</b>
+                        </p>
+                           We use cookies on our website to improve your user experience. Cookies are small text files that are placed on your device by a website. We use cookies to remember your preferences, to analyze user behavior, and to provide personalized content and ads.
+
+                           You can control the use of cookies through your browser settings. However, if you disable cookies, some features of our website may not function properly.
+                        <p>
+                        <br/>
+                          <b> Third-Party Links: </b>
+                        </p>
+                           Our website may contain links to third-party websites that are not under our control. We are not responsible for the privacy practices or content of these websites. We encourage you to review the privacy policies of these websites before using them.
+                        <p>
+                        <br/>
+                          <b> Changes to This Policy: </b>
+                        </p>
+                           We reserve the right to modify this privacy policy at any time. We will notify you of any significant changes by posting a notice on our website or by sending you an email.
+                        <p>
+                        <br/>
+                           <b> Contact Us: </b>
+                        </p>
+                           If you have any questions or concerns about our privacy policy or our use of your personal information, please contact us at 678-999-8212
+                </>}
+                handleClose = {togglePopup2}
+              />}
             </Grid>
             <Button
               //button is disabled until valid input of a valid email and valid password
-              disabled = {!validEmail || !validPwd  ? true : false}
+              disabled = {!validEmail || !validPwd || !validUser ? true : false}
               type="submit"
               fullWidth
               variant="contained"
