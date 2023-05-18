bdd02e16450f062b788b3f8f5a09e30eebc111d4
ethan <eth4n1@gmail.com>
2023-05-18 11:53:01 -0700

    Updated About.js

--- Gettr/src/components/About.js
+++ Gettr/src/components/About.js
@@ -1,7 +1,37 @@
 import React from 'react';
 import ResponsiveAppBar from './Nav'
+import Typography from '@mui/material/Typography';
+import { Box } from '@mui/material'
+
 const About =()=> {
-    return(<div><ResponsiveAppBar/>deez</div>);
+    return(
+        <>
+        <ResponsiveAppBar/>
+<Box
+          sx={{
+            marginTop: 40,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+         <Typography component="h1" variant="h4">
+            About GETTR
+         </Typography>
+          <Typography component="subtitle1">
+            <p>
+            Gettr is a web forum app to help facilitate project collaboration between students and faculty at different universities.
+            <br/>
+            Developed by "THE ELITE 4", our team strives to make quality software that bridges the communication gap
+            <br/>
+            for our clients and serves as a place where they can easily discuss what projects they wish to create,
+            <br/>
+            but also find others who will aid them in turning those ideas into actual projects.
+
+            </p>
+          </Typography>
+         </Box>
+         </>);
 }
 
 export default About;
